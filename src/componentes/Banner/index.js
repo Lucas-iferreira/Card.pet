d8e696d391d7c64875ed8d51605173cf4076162b
Lucas-iferreira <114694509+Lucas-iferreira@users.noreply.github.com>
2023-02-06 15:04:38 -0300

    adicionando o banner

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,11 +1,14 @@
-import './Banner.css'
+import './Banner.css';
 
 function Banner() {
-    return(
-        <header className="banner">
-        <img src="/imagens/bannerppet.png" alt ="O banner principal da página do Organo"/>
-        </header>
-    )
+  return (
+    <header className="banner">
+      <img
+        src="/imagens/banner.png"
+        alt="O banner principal da página do Organo"
+      />
+    </header>
+  );
 }
 
-export default Banner 
+export default Banner;
